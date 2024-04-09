d149580090c84fe83286d8f48c9c98f1bfc1267c
penalemesc <165402506+penalemesc@users.noreply.github.com>
2024-04-08 22:16:36 -0500

    Update earthquakequery.sql

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -4,11 +4,11 @@ SELECT *
   WHERE place LIKE '%Argentina%';
 
 --find the magnitude of any earthquake that happened at an latitude of at least 20
-SELECT AVG(mag) 
+SELECT mag 
   FROM earthquakes e 
   WHERE e.latitude >= 20;
 
 --FInd how many earthquakes there where in the dataset and print the average magnitude
-SELECT count(quaketime), (SELECT AVG(mag) FROM earthquakes) AS averageStrength 
+SELECT count(quaketime) AS amountOfEarthquakes, (SELECT AVG(mag) FROM earthquakes) AS averageStrength 
   FROM earthquakes
   GROUP BY (averageStrength);
