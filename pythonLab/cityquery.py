49bf0f48dc50aabd728a7662a972858132888e40
penalemesc <conradoplemes@gmail.com>
2024-04-22 11:47:53 -0400

    Modified cityquery.py

--- pythonLab/cityquery.py
+++ pythonLab/cityquery.py
@@ -1,3 +1,10 @@
+"""
+cityquery.py
+Created by @author Conrado Peña Lemes
+Returns queries requested in the assignment pyhtonLab
+Queries were originally created in sql, that is why queries such as number 4 are so long
+"""
+
 import psycopg2
 
 def query1():
@@ -12,7 +19,8 @@ def query1():
 
 	sql = "select lot, lat from USCitiesTop1k where city = 'Northfield';"
 
-	row = cur.execute (sql)
+	cur.execute(sql)
+	row = cur.fetchall()
 
 	if row is None:
 		print("Sorry northfield is not on the Database.")
@@ -33,9 +41,10 @@ def query2():
 	cur = conn.cursor()
 
 	sql = "WITH maxCityPop AS (SELECT max(cityPop) AS MaxCP FROM USCitiesTop1k) SELECT city AS BiggestCity FROM USCitiesTop1k, maxCityPop WHERE cityPop = MaxCP;"
-
-	rowList = cur.execute(sql)
-
+	cur.execute(sql)
+	
+	rowList = cur.fetchall()
+	
 	return rowList
 
 def query3():
@@ -59,9 +68,12 @@ def query3():
           minCityPop
     WHERE cityPop = MinCP;"""
 
-	rowList = cur.execute(sql)
+	cur.execute(sql)
+	rowList = cur.fetchall()
 	return rowList
 
+#query 4 does not return anything, the reason for this I am unsure of as the sql statement does work
+#perhaps it would work if I had a way to make python run an sql file instead of having the entire query in python
 def query4():
 	conn = psycopg2.connect(
         host="localhost",
@@ -120,6 +132,30 @@ DROP VIEW if EXISTS furthestSouth;
     WHERE UCT.lat = FS.FSouth;"""
 
 	cur.execute(sql)
+	cur.fetchall()
+
+def query5():
+	conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="penalemesc",
+        user="penalemesc",
+        password="bird739square")
+
+	cur = conn.cursor()
+	print("Hi please write a State or state abbreviation to look for in the database")
+	input1 = input()
+	if input1.count == 2:
+		sql = "select stateName, staPop from USStatesPop where code = %s"
+		cur.execute(sql, [input1.capitalize])
+		cur.fetchall()
+	else:
+		sql = "select staPop from USStatesPop where stateName = %s"
+		cur.execute(sql, [input1])
+		cur.fetchall()
+		
+
+	
 
 	
 
@@ -127,4 +163,5 @@ print (query1())
 print (query2())
 print (query3())
 print (query4())
+print (query5())
 
