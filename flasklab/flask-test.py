36f83fcb9145536f112097e611af7bbefa9a483b
penalemesc <conradoplemes@gmail.com>
2024-04-24 11:38:25 -0400

    Finished part 1 of the lab entirely

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,4 +1,9 @@
+"""
+Edited by Conrado Peña Lemes
+Uses flask to return queries on a website
+"""
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -7,14 +12,40 @@ app = flask.Flask(__name__)
 def my_function():
     return "Hello World!"
 
+#Esto funciona porque le damos parametros 2 palabras que en este caso son random y las devuelve, pero
+#se podria usar con palabras especificas para que muestre una cosa especifica
 @app.route('/display/<word1>/<word2>')
 def my_display(word1, word2):
-    the_string = "The words are: " + word1 + " and " + word2;
+    the_string = "The words are: " + word1 + " and " + word2
     return the_string
 
 @app.route('/color/<word1>')
 def my_color(word1):
     return '<h1 style="color:Red">' + word1 + '</h1>'
+
+@app.route('/add/<num1>/<num2Str>')
+def addTwoNums(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    added = num1 + num2
+    added = str(added)
+    return added
+
+@app.route('/pop/<abbrev>')
+def populationCount(abbrev):
+    
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="penalemesc",
+        user="penalemesc",
+        password="bird739square")
+    
+    cur = conn.cursor()
+    sql = "select staPop from USStatesPop where code = %s"
+    cur.execute(sql, [abbrev.upper()])
+    return cur.fetchall()
+    
 
 if __name__ == '__main__':
     my_port = 5104
