c20e713a217680ff44fb30b0682b0db70366f18b
penalemesc <conradoplemes@gmail.com>
2024-05-16 17:38:18 -0400

    Finished cssLab

--- csslab/lab/cssLab.py
+++ csslab/lab/cssLab.py
@@ -1,35 +1,33 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import psycopg2
-import random
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    message = "Hello! Here you can get a random sentence by use of an sql Query about when some fruits where first used as a weapon"
-    message = message + " And change the colour to the background of the page to whatever colour you write"
-    
-    conn = psycopg2.connect(
+
+@app.route('/',methods=['GET', 'POST'])
+def citiesInState():
+    if request.method == 'POST':
+        staCode = request.form['stateCode']
+        conn = psycopg2.connect(
         host="localhost",
         port=5432,   
         database="penalemesc",
         user="penalemesc",
         password="bird739square")
-    
-    cur = conn.cursor()
-    num = random.randint(1, 5)
-    rYear = random.randint(-9999, 9999)
-    print(num)
-    sql = "select rName from randomN where rNum = %s"
-    cur.execute(sql, [num])
-    #The only caveat that I have with this is that I am not aware of how to fetch the data and not the () that come
-    #with the fetchone function, but everything else works
-    gh = cur.fetchone()
-    
-	
-    return render_template("paginaInicial.html", randFood = gh[0], randYear = rYear)
 
+        cur = conn.cursor()
+        sql = "select city from USCitiesTop1k USC, USStatesPop USS where USC.stateName = USS.stateName and code = %s"
+        cur.execute(sql, [staCode.upper()])
+        ciudad = cur.fetchall()
+        #If writing a state that doesnt exist, it will return what we wrote and an empty array
+        return render_template("CitiesInState.html", cities = ciudad, state = staCode)
+    
+    else:
+        return render_template("CitiesInState.html")
+    
 
 if __name__ == '__main__':
     my_port = 5104
--- csslab/lab/static/lab.js
+++ csslab/lab/static/lab.js
@@ -1,15 +1,14 @@
-function rSentece(){
-	
-}
-
-colourButton = document.getElementById("colourButton");
-function backgColorChange(){
-	if (invertion == false){
-  	colourButton.style.webkitFilter = "invert(100%)";
+//If its not inverted it inverts it
+invertion = false;
+document.getElementById('mouseOverPara').onmouseover = function() {
+  if (invertion == false){
+  	this.style.webkitFilter = "invert(100%)";
     invertion = true;
   }
 	else {
-  	colourButton.style.webkitFilter = "invert(0)";
+  	this.style.webkitFilter = "invert(0)";
     invertion = false;
   }
-}
+};
+
+
